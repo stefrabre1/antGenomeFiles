3929132eaa18c0f7f98babf4fc40edd8afc2330a
Stephen Personal <stefrabre@gmail.com>
2022-10-17 21:41:31 -0500

    small changes to commenting in files

--- vcfToMst.py
+++ vcfToMst.py
@@ -8,8 +8,7 @@ import pandas as pd
 
 parser = argparse.ArgumentParser(description='Organizes data from VCF into a format which can be read by MSTLinkage.')
 parser.add_argument('--header', help='header file with options for how the program is run', default='v2mHeader.txt')
-parser.add_argument('--output', help='filename for the output file', default='readyForMCT')
-parser.add_argument('--deleteIndex', help='deletes the first column of the file (y/n)', default="y")
+parser.add_argument('--output', help='filename for the output file', default='readyForMST.txt')
 parser.add_argument('--debug', help='print debug output', action="store_true")
 args = parser.parse_args()
 
@@ -25,8 +24,15 @@ with open(args.header) as headerFile:
         
 #TODO: can make this more robust by finding "# INPUT FILES" and then grabbing filenames from that index
 file012 = pd.read_csv(headerContent[indx012], sep='\t', header = None)
-fileIndv = pd.read_csv(headerContent[indxIndv], sep='\t', header = None)
-filePos = pd.read_csv(headerContent[indxPos], sep='\n', header = None)
+
+# remove any extensions from Indv TODO: (maybe make this an option in header)
+noExt = ['locus']
+with open(headerContent[indxIndv]) as indvExt:
+    for line in indvExt:
+        temp = line.split('.')
+        noExt.append(temp[0])
+fileIndv = pd.DataFrame({None:noExt})
+#fileIndv = pd.read_csv(headerContent[indxIndv], sep='\t', header = None) TODO: keeping this until I can make it optional
 headerFile.close()
 
 # delete the index of the 012 file and transpose
@@ -35,6 +41,7 @@ file012T = file012.transpose()
 file012T.reset_index(drop=True, inplace=True)
 
 # replace tab with underscore in filePos 
+filePos = pd.read_csv(headerContent[indxPos], sep='\n', header = None)
 filePosUnder = filePos.replace(to_replace='\t', regex=True, value='_')
 
 # make filePosUnder first column in file012T
@@ -53,13 +60,14 @@ alphaInverseFile = file012T.replace(to_replace=[-1, 0, 1, 2], value=['U', 'B', '
 combo = pd.concat([alphaFile, alphaInverseFile])
 combo.reset_index(drop=True, inplace=True)
 
-#TODO: strip (.all.bam) extension
+# TODO: Make these optional if user doesn't want extension stripping
+#fileIndvIns = pd.DataFrame([['locus']], columns=fileIndv.columns)
+#fileIndv2 = pd.concat([fileIndvIns, fileIndv])
+#fileIndv2.reset_index(drop=True, inplace=True)
+#fileIndvT = fileIndv2.transpose()
 
 # insert top row using transposed fileIndv
-fileIndvIns = pd.DataFrame([['locus']], columns=fileIndv.columns)
-fileIndv2 = pd.concat([fileIndvIns, fileIndv])
-fileIndv2.reset_index(drop=True, inplace=True)
-fileIndvT = fileIndv2.transpose()
+fileIndvT = fileIndv.transpose()
 headerless = pd.concat([fileIndvT, combo])
 headerless.reset_index(drop=True, inplace=True)
 
@@ -82,3 +90,4 @@ with open(args.output, 'x') as toWrite:
     toWrite.writelines(MCTHeader)
 toWrite.close()
 headerless.to_csv(args.output, mode='a', sep='\t', header=None, index=None)     
+#combo.to_csv(args.output, mode='a', sep='\t', header=None, index=None)
