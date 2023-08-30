5257cfdaafb21e3492f57cf623aed33c01344a82
Stephen Personal <stefrabre@gmail.com>
2023-08-30 11:32:11 -0700

    Made it possible to use a file with scaffold names for extraction

--- scaffExtractor.py
+++ scaffExtractor.py
@@ -3,34 +3,34 @@
 # script for filtering out individuals with high proportions of missing data (VCF files)
 
 import os
-#import re
 import argparse
 
 # Set up a very helpful argument parser ecosystem to read in arguments passed through on the command line
 parser = argparse.ArgumentParser(
-    description='script for filtering out individuals with high proportions of missing data (VCF files)',
+    description='script for extracting specified scaffolds from a file',
     epilog="Good Luck with Your Bioinformatics! This script is written and directed by Daniela Zarate PHD")
-parser.add_argument('--debug', help='print debug output', action='store_true')
 parser.add_argument('--input', help='file to read from')
-parser.add_argument('--scaffs', help='list of scaffolds to extract/exclude')
-parser.add_argument('--invert', help='extract all but named scaffold (y/n)', default='n')
-
+parser.add_argument('--output', help='file to write to', default = "extractedScaffs")
+parser.add_argument('--scaffFile', help='a file containing a list of scaffolds to extract/exclude')
+parser.add_argument('--debug', help='print debug output', action='store_true')
 
 ## Global Variables
 
 args = parser.parse_args()
-output = args.input + ".scaffExtract"
-scaffList = args.scaffs.split()
+
+scaffList = []
+with open (args.scaffFile, 'r') as file:
+    for line in file:
+        scaffList.append(line.strip())
+        
+#test 
+print(scaffList)
 
 ## Main
 
 # Read input file, write header and all desired lines to file
-with open(args.input, 'r') as file, open(output, 'w+') as outFile:
+with open(args.input, 'r') as file, open(args.output, 'w+') as outFile:
     for line in file:
-        if args.invert == 'y':
-            if line.split()[0] == "@SQ" or line.split()[0] == "@PG" or line.split()[2] not in scaffList:
-                outFile.write(line)
-        else:    
-            if line.split()[0] == "@SQ" or line.split()[0] == "@PG" or line.split()[2] in scaffList:
-                outFile.write(line)
+        if line.split()[0] == "@SQ" or line.split()[0] == "@PG" or line.split()[2] in scaffList:
+            outFile.write(line)
     
